af2b234d038e90230573bf147965a778b4b3f2f6
Samuel Faulkner <samuelf@cs.utexas.edu>
2018-10-02 17:46:17 -0500

    Adding the Wall.cpp file to ball_setup

--- appdirectory/Wall.cpp
+++ appdirectory/Wall.cpp
@@ -1,12 +1,16 @@
 #include "Wall.h"
+#include <OgreRoot.h>
+#include <OgreEntity.h>
+#include <OgreMeshManager.h>
+#include <btBulletCollisionCommon.h>
 
 Wall::Wall(Ogre::String name, 
     Ogre::SceneManager* sceneMgr, 
     Physics* simulator,
     Ogre::Vector3 direction) {
-    this.name = name;
-    this.sceneMgr = sceneMgr;
-    this.simulator = simulator;
+    this->name = name;
+    this->sceneMgr = sceneMgr;
+    this->simulator = simulator;
     
     Ogre::MovablePlane plane(direction, -GRID_SIZE/2);
     Ogre::MeshManager::getSingleton().createPlane(
@@ -15,20 +19,19 @@ Wall::Wall(Ogre::String name,
         direction.perpendicular()
     );
 
-    rootNode = this.sceneMgr->getRootSceneNode()->createChildSceneNode();
-    geom = this.sceneMgr->createEntity(name);
+    rootNode = this->sceneMgr->getRootSceneNode()->createChildSceneNode();
+    geom = this->sceneMgr->createEntity(name);
     rootNode->attachObject(geom);
 
-    wallEntity->setMaterialName("sibenik/poplocenje");
+    geom->setMaterialName("sibenik/poplocenje");
     Ogre::MaterialPtr mp = Ogre::MaterialManager::getSingleton().getByName("sibenik/poplocenje");
     mp.get()->setReceiveShadows(true);
-    wallEntity->setCastShadows(false);
+    geom->setCastShadows(false);
 
-    /*shape = new btPlaneShape(btVector3(direction.x, direction.y, direction.z), 0.0f);
+    btVector3 btDir = btVector3(direction.x, direction.y, direction.z);
+    tr.setOrigin(-GRID_SIZE * btDir);
+    shape = new btBoxShape(btVector3(GRID_SIZE/2, GRID_SIZE/2, GRID_SIZE/2));
     mass = 0.0;
-    tr = 
     motionState = new OgreMotionState(tr, rootNode);
-    body = btRigidBody(mass, motionState, shape);
-    
-    inertia =*/
+    body = new btRigidBody(mass, motionState, shape);
 }
